69aecfcaf8b5d7e17c3414f9e343fc7b8287bca5
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-07-07 13:38:26 -0300

    alterações no data

--- src/data.js
+++ src/data.js
@@ -1,4 +1,6 @@
-// estas funciones son de ejemplo
+import data from '.data/ghibli/ghibli.json';
+
+/* estas funciones son de ejemplo
 
 export const example = () => {
   return 'example';
@@ -6,4 +8,56 @@ export const example = () => {
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
+
+
+//Função de filtrar filme
+
+//Função de filtrar personagem A-Z
+const filtros = {
+  
+  //ou personagensAZ: function (dataName) {
+    //dataName.sort()
+  personagensAZ: function (dataName) {
+    dataName.sort((A,Z) => {
+      if (A > Z) {
+        return 1;
+      }
+      else if (A < Z) {
+        return -1;
+      }
+      else {
+        return 0;
+      }
+    });
+  },
+
+  personagensIdade: function (dataAge) {
+    dataAge.sort((a,b) => {
+      if (a > b) { 
+        return 1;
+      }
+      else if (a < b) {
+        return -1;
+      }
+      else { 
+        return 0;
+      }
+    });
+  },
+
+  personagensGenero: function (dataGender) {
+    dataGender.filter((Female, Male) => {
+      if (dataGender === Female) {
+        return true;
+      } 
+      else {
+        return false;
+      }
+    });
+  },
+
+}
+
+export default filtros
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { filtros } from './data.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+//console.log(example, data);
+
+//Filmes
+
+function mostrarCards(data) { 
+    document.getElementById('filmes').innerHTML = data.map((item) =>
+        `<div class="filmes">
+        <div class="img-poster"> 
+            <img src="${item.poster}"> 
+        </div>
+        <div class="titulo">${item.title}
+        </div> 
+        </div>`
+    );
+}
+
+//Personagens
+
+const filmes = data.films;
+
+function showCardsCharacters (characters, film){
+    
+    characters.forEach(people =>
+        document.getElementById('pessoas').innerHTML += 
+        `<div class="img-personagem">
+            <img src="${people.img}">
+        </div>
+        <div class="nome-personagem">${people.name}</div>`           
+    );
+}
+for (let film of filmes){
+    let characters = film.people;
+    showCardsCharacters(characters);
+}
+mostrarCards(data.films);
