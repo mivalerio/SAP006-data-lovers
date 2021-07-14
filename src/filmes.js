b270c3aa63990ce74f7f3daa705929d53d3046e6
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-07-14 13:51:18 -0300

    Alterções filtro

--- src/filmes.js
+++ src/filmes.js
@@ -2,23 +2,21 @@ import data from './data/ghibli/ghibli.js';
 
 
 function mostrarCards(data) { 
-    document.getElementById('cardsFilmes').innerHTML = data.map((item) => ` 
+    document.getElementById('cards-filmes').innerHTML = data.map((item) => ` 
         <div class="filmes">
             <div class="img-poster"> 
                 <img src="${item.poster}"> 
-                <div class="titulo">${item.title}</div>
+                <div class="titulo-filmes">${item.title}</div>
             </div> 
         </div>`
-        
-        
-    
-    ) 
+ 
+    ).join("")
 } 
 mostrarCards(data.films); 
 
 
 
-function mostrarInformacoesCards(data) {
+/*function mostrarInformacoesCards(data) {
     document.getElementById('cardsInformacoes').innerHTML = data.map((item) => `
     <div class="cardBack">
         <ul class="informacoes">
@@ -27,11 +25,11 @@ function mostrarInformacoesCards(data) {
           <li>Ano de Lançamento:${item.release_date}</li>
           <li>RT Score:${item.rt_score}</li>
         </ul>
-    </div>
-    `)
+    </div>`
+    )
 }
 
-mostrarInformacoesCards(data.films);
+mostrarInformacoesCards(data.films);*/
 
 
 
--- src/personagens.js
+++ src/personagens.js
@@ -1,19 +1,84 @@
+import { orders, genderFilterFemale, genderFilterMale } from './data.js';
 import data from './data/ghibli/ghibli.js'; 
 
+//Entrar na array de persongem
+
 const filmes = data.films;
-function showCardsCharacters(characters, film) {
-    characters.forEach(people => 
-        document.getElementById('pessoas').innerHTML += 
-        `<div class="img-personagem"> 
-        <img src="${people.img}"> 
-        </div> 
-        <div class="nome-personagem">${people.name}</div>`
-        
-    );
+const pessoas = document.getElementById("card-characters");
+
+export function getCharacters(){
+    let characters = [];
+    for (let film of filmes){
+        for(let people of film.people){
+            characters.push(people);
+        }
+    }
+    return characters
 }
- 
-for(let film of filmes) { 
-    let characters = film.people; 
-    showCardsCharacters(characters); 
+
+//Printar filmes na tela
+
+function showCardsCharacters (characters){
+    pessoas.innerHTML = "";
+    characters.map((people) => {
+        pessoas.innerHTML +=
+        `<div class="filmes">
+            <div class="img-personagem"> 
+                <img src="${people.img}"> 
+                <div class="nome-personagem">${people.name}</div>
+            </div> 
+        <div>`
+    }).join("");
+}
+
+showCardsCharacters(getCharacters())
+
+
+//ordens/filtros
+
+//AZ
+const order = document.getElementById("order");
+
+function alphabetOrder(event){
+    const option = event.target.value
     
+    if (option == "az"){
+        showCardsCharacters(orders.charactersOrderAZ());
+    }
+    else {
+        showCardsCharacters(orders.charactersOrderZA());
+    }
 }
+order.addEventListener("change",alphabetOrder)
+
+//Idade
+const charactersAge = document.getElementById("age");
+
+function ageOrder(event){
+    const option = event.target.value
+    
+    if (option == "older-to-younger"){
+        showCardsCharacters(orders.charactersAgeOldertoYounger());
+    }
+    else {
+        showCardsCharacters(orders.charactersAgeYoungerToOlder());
+    }
+
+}
+charactersAge.addEventListener("change", ageOrder)
+
+//Genero
+const characterGender = document.getElementById("gender")
+
+function genderFilter(event){
+    const option = event.target.value
+    
+    if (option == "women"){
+        showCardsCharacters(getCharacters(genderFilterFemale()));
+    }
+    else {
+        showCardsCharacters(getCharacters(genderFilterMale()));
+    }
+}
+characterGender.addEventListener("change", genderFilter)
+
