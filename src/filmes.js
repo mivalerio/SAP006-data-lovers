bfffe6d40d5a4ca339427c7f8ae300d63568b678
Michele Martins <michele.valerio@outlook.com.br>
2021-07-19 18:54:32 -0300

    implementaçao da janela modal

--- src/filmes.js
+++ src/filmes.js
@@ -3,42 +3,67 @@ import { ordenarDatadeLancamento } from './data.js';
 import { ordenaMaiorScore } from './data.js';
 import { ordenaAZ } from './data.js';
 import { filtraNome } from './data.js';
+import { filtraDiretor } from './data.js';
 
 const filmes = data.films;
 const pesquisar = document.getElementById("busca");
+const cardsFilmes = document.getElementById("cards-filmes");
+const cardsInformacoes = document.querySelector(".cards-informacoes");
+const popup = document.querySelector(".popup-wrapper");
+const btnFechar = document.querySelector(".popup-close");
+const media = document.querySelector("media");
 
 function mostrarCards(data) {
-    document.getElementById('cards-filmes').innerHTML = data.map((item) => ` 
+    cardsFilmes.innerHTML = "";
+    const teste = data.map((item) => {
+        const card = document.createElement("div")
+        card.innerHTML = ` 
         <div class="filmes">
-            <div class="img-poster"> 
+            <div class="img-poster">    
                 <img src="${item.poster}"> 
-                <div class="titulo-filmes">${item.title}</div>
-            </div> 
-        </div>`
- 
-    ).join("")
-} 
-mostrarCards(data.films); 
+                <H3>${item.title}</H3>
+            </div>
+                    
+        </div>`,
+            cardsFilmes.appendChild(card),
+
+            card.addEventListener('click', () => {
+                popup.style.display = 'block'
+
+                cardsInformacoes.innerHTML = `
+            
+                <div class="filmes">
+                    <ul class="informacoes">
+                    <li>Descrição:${item.description}</li>
+                    <li>Diretor:${item.director}</li>
+                    <li>Ano de Lançamento:${item.release_date}</li>
+                    <li>RT Score:${item.rt_score}</li>
+                    </ul>
+                </div>`
 
 
+            })
 
-/*function mostrarInformacoesCards(data) {
-    document.getElementById('cardsInformacoes').innerHTML = data.map((item) => `
-    <div class="cardBack">
-        <ul class="informacoes">
-          <li>Descrição:${item.description}</li>
-          <li>Diretor:${item.director}</li>
-          <li>Ano de Lançamento:${item.release_date}</li>
-          <li>RT Score:${item.rt_score}</li>
-          <li>Localização:${item.locations.name}</li>
-          <li>Clima:${item.climate}</li>
-          <li>Terreno:${item.terrain}</li>
-        </ul>
-    </div>`
-    )
+
+    }).join("")
+
 }
+mostrarCards(data.films);
 
-mostrarInformacoesCards(cardBack);*/
+//mostrar informações no card
+cardsFilmes.addEventListener('click', () => {
+    popup.style.display = 'block'
+})
+
+//botaoFechar da popup
+btnFechar.addEventListener('click', () => {
+    popup.style.display = 'none'
+})
+
+//fechar a tela ao clicar fora do popup
+popup.addEventListener('click', () => {
+    popup.style.display = 'none'
+})
 
 
 const selectElement = document.querySelector('.opcao');
@@ -49,10 +74,10 @@ selectElement.addEventListener('change', (event) => {
     const opcaoSelecionada = event.target.value;
     const result = document.querySelector('.cards-filmes');
 
-    
+
     if (opcaoSelecionada == "release_date") {
         const filmesOrdenados = ordenarDatadeLancamento(filmes)
-        console.log("Oi Deu Certo!");
+
 
         mostrarCards(filmesOrdenados);
 
@@ -60,23 +85,31 @@ selectElement.addEventListener('change', (event) => {
 
     if (opcaoSelecionada == "rt_score") {
         const ordenaScoreMaior = ordenaMaiorScore(filmes)
-        console.log("Ordenou Maior Score");
+
 
         mostrarCards(ordenaScoreMaior);
     }
-    
+
     if (opcaoSelecionada == "title") {
         const ordenaNome = ordenaAZ(filmes)
-        console.log("Ordenou nome");
+
 
         mostrarCards(ordenaNome);
     }
 
     if (opcaoSelecionada == "title") {
         const ordenaNome = ordenaAZ(filmes)
-        console.log("Ordenou nome");
 
         mostrarCards(ordenaNome);
+
+    }
+
+    if (opcaoSelecionada == "director") {
+        const ordenaDiretor = filtraDiretor(filmes)
+        console.log("Ordenou diretor");
+
+        mostrarCards(ordenaDiretor);
+
     }
 
 
@@ -94,18 +127,16 @@ selectElement.addEventListener('change', (event) => {
     document.getElementById("director").value= "";
     }*/
 
-    
+
 //filtrar pelo input
 
 function procurarNome(e) {
     e.preventDefault();
-    mostrarCards(filtraNome(data.films, pesquisar.value));    
-    
+    mostrarCards(filtraNome(data.films, pesquisar.value));
+
 }
 
 pesquisar.addEventListener("keyup", procurarNome);
-
-
 
 
 
