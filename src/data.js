4b125e19eac96c44d17b93c065ce89ed90b58895
Michele Martins <michele.valerio@outlook.com.br>
2021-07-26 18:48:48 -0300

    implementaçao dos testes unitarios

--- src/data.js
+++ src/data.js
@@ -1,6 +1,5 @@
-import data from './data/ghibli/ghibli.js';
+import data from "./data/ghibli/ghibli.js";
 //import { getCharacters } from './personagens.js';
-
 
 export const ordenarDatadeLancamento = (films) => {
   const filmesOrdenados = films.sort(function (a, b) {
@@ -14,7 +13,7 @@ export const ordenarDatadeLancamento = (films) => {
     return 0;
   });
   return filmesOrdenados;
-}
+};
 
 export const ordenaMaiorScore = (films) => {
   const ordenaScoreMaior = films.sort(function (a, b) {
@@ -28,7 +27,7 @@ export const ordenaMaiorScore = (films) => {
     return 0;
   });
   return ordenaScoreMaior;
-}
+};
 
 export const ordenaAZ = (films) => {
   const ordenaNome = films.sort(function (a, b) {
@@ -42,158 +41,135 @@ export const ordenaAZ = (films) => {
     return 0;
   });
   return ordenaNome;
-}
-
-export const filtraDiretor = (films) => {
-  const ordenaDiretor = films.sort(function (a, b) {
-    if (a.director = b.director) {
-      return 1;
-    }
-  });
-  return ordenaDiretor;
-}
-
-// Filtrar por nome através do input text
-export const filtraNome = (data,films) => {
-  
-  let procuraPorNome = data.filter((filtraNome =>
-    filtraNome.title.toLowerCase().includes(films.toLowerCase())
-    
-    ));
-    return procuraPorNome;
 };
 
+export const filtraDiretor = (films, director) => {
+  const ordenaDiretor = films.filter((films) => films.director === director);
+
+  return ordenaDiretor;
+};
+
+// Filtrar por nome através do input text
+export const filtraNome = (data, films) => {
+  let procuraPorNome = data.filter((filtraNome) =>
+    filtraNome.title.toLowerCase().includes(films.toLowerCase())
+  );
+  return procuraPorNome;
+};
 
 //calcular media Score
 
-const filmes = data.films;
+export const mediaNotas = (arrayScores) => {
+  const mediaFilmes = arrayScores.reduce((a, b) => a + b) / arrayScores.length;
 
-let media = [];
+  return mediaFilmes;
 
-for (let i = 0; i < filmes.length; i++) {
-media.push(parseInt(filmes[i]["rt_score"]));
-}
-  
-export const mediaNotas = media.reduce((a, b) =>
-(a + b)) / media.length;
 
-console.log(media.length);
-console.log(mediaNotas);
-  
+};
+
 //Personagens
 //Funções de ordenar personagem
 
-export const orders = {
-  
+/*export const orders = {
   //A-Z
 
-  charactersOrderAZ: function() {
-    const character = getCharacters()
-    character.sort((A,Z) => {
+  charactersOrderAZ: function () {
+    const character = getCharacters();
+    character.sort((A, Z) => {
       if (A.name > Z.name) {
         return 1;
       }
       if (A.name < Z.name) {
         return -1;
-      }
-      else {
+      } else {
         return 0;
       }
-      
     });
-    return character;                      
+    return character;
   },
 
   //Z-A
 
-  charactersOrderZA: function() {
-    const character = getCharacters()
-    character.sort((A,Z) => {
+  charactersOrderZA: function () {
+    const character = getCharacters();
+    character.sort((A, Z) => {
       if (A.name > Z.name) {
         return -1;
       }
       if (A.name < Z.name) {
         return 1;
-      }
-      else {
+      } else {
         return 0;
       }
-      
     });
     return character;
   },
-  
+
   //Mais novo para o mais velho
 
-  charactersAgeYoungerToOlder: function() {
-    const character = getCharacters()
-    character.sort((a,b) => {
-      if (a.age > b.age) { 
+  charactersAgeYoungerToOlder: function () {
+    const character = getCharacters();
+    character.sort((a, b) => {
+      if (a.age > b.age) {
         return 1;
-      }
-      else if (a.age < b.age) {
+      } else if (a.age < b.age) {
         return -1;
-      }
-      else { 
+      } else {
         return 0;
       }
-
     });
     return character;
   },
-  
+
   //Mais velho para o mais novo
 
-  charactersAgeOldertoYounger: function() {
-    const character = getCharacters()
-    character.sort((a,b) => {
-      if (a.age > b.age) { 
+  charactersAgeOldertoYounger: function () {
+    const character = getCharacters();
+    character.sort((a, b) => {
+      if (a.age > b.age) {
         return -1;
-      }
-      else if (a.age < b.age) {
+      } else if (a.age < b.age) {
         return 1;
-      }
-      else { 
+      } else {
         return 0;
       }
-
     });
     return character;
-  }
-
-}
+  },
+};
 
 //Funções de filtar
 
-export const filters ={
-  
+export const filters = {
   //Mulher
-  
-  genderFilterFemale: function() {
-    const character = getCharacters()
-    return character.filter((people) => people.gender === "Female")
-  }, 
+
+  genderFilterFemale: function () {
+    const character = getCharacters();
+    return character.filter((people) => people.gender === "Female");
+  },
 
   //Homem
 
-  genderFilterMale: function(){
-    const character = getCharacters()
-    return character.filter(people => people.gender === "Male")
+  genderFilterMale: function () {
+    const character = getCharacters();
+    return character.filter((people) => people.gender === "Male");
   },
-  
+
   //Outros
 
-  genderFilterOther: function() {
-    const character = getCharacters()
-    return character.filter((people => people.gender !=="Female" && people.gender !== "Male"))
-  }
-
-}
+  genderFilterOther: function () {
+    const character = getCharacters();
+    return character.filter(
+      (people) => people.gender !== "Female" && people.gender !== "Male"
+    );
+  },
+};
 
 //Calculo
-export function percentageGender(gender){
-  const character = getCharacters().length
-  const percentage = []
-  console.log(percentage)
-  return percentage = (100*gender.length)/character
+export function percentageGender(gender) {
+  const character = getCharacters().length;
+  const percentage = [];
+
+  return (percentage = (100 * gender.length) / character);
 }
+*/
